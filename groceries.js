d4b497e475b249e434dec30fd4148a968b890d99
rlynn1 <rlynn1@mail.csuchico.edu>
2017-04-04 15:02:11 -0700

    Create List
    Set up the javascript to copy inputs onto the page to create a list. After the button is pressed, the input is then deleted from the text box.

--- groceries.js
+++ groceries.js
@@ -1,16 +1,25 @@
-function addItem(){
-
+function addItem()
+{
+    var input = document.getElementById("newItem").value;
+    var list = document.getElementById("listDisplay");
+    var item = document.createElement("li");
+    var itemName = document.createTextNode(input);
+    item.appendChild(itemName);
+    list.appendChild(item);
+    document.getElementById("newItem").value = "";
 }
 
 //courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
-function setCookie(cname, cvalue, exdays) {
+function setCookie(cname, cvalue, exdays)
+{
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires="+d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 //courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
-function getCookie(cname) {
+function getCookie(cname)
+{
     var name = cname + "=";
     var ca = document.cookie.split(';');
     for(var i = 0; i < ca.length; i++) {
