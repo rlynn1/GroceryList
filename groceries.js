a48e3c6be3264cb3f0bd3c7d66840663d3027cb9
Kevin Buffardi <kbuffardi@csuchico.edu>
2017-04-04 12:16:42 -0700

    Back to beginning of lab

--- groceries.js
+++ groceries.js
@@ -1,45 +1,7 @@
 function addItem(){
 
 }
-function createRemoveButton(){
-  var close = document.createElement("button");
-  close.classList.add("btn");
-  close.classList.add("btn-danger");
-  close.classList.add("btn-xs");
-  //close.appendChild(document.createTextNode("X"));
-  var icon = document.createElement("span");
-  icon.classList.add("glyphicon");
-  icon.classList.add("glyphicon-remove");
-  close.appendChild(icon);
-  return close;
-}
 
-function removeListItem(){
-  console.log("Item: " + this);
-  console.log("Parent: " + this.parentNode);
-  var mom = this.parentNode;
-  var itemRemove = mom.firstChild.textContent;
-  var itemIndex = myList.indexOf(itemRemove);
-  myList.splice(itemIndex,1);
-  var grandma = mom.parentNode;
-  grandma.removeChild(mom);
-}
-
-function saveList()
-{
-  setCookie("savedList",myList.toString(),1);
-  var saved = getCookie("savedList");
-}
-
-function openList()
-{
-  var open = getCookie("savedList");
-  var list = open.split(",");
-  for(var i=0; i<list.length; i++)
-  {
-    console.log(list[i]);
-  }
-}
 //courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
